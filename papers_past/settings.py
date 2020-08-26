4df7ae602acb6e4c7940ed7c2e4e0aa1bd764632
Caleb Moses <caleb@dragonfly.co.nz>
2020-08-26 22:47:00 +1200

    Set download_delay = 0.1

--- papers_past/settings.py
+++ papers_past/settings.py
@@ -24,7 +24,8 @@ CONCURRENT_REQUESTS = 4
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.1
+
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
