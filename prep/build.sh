e8636d88fdb61b5e8e6870f1cdef720db55fb243
Caleb Moses <caleb@dragonfly.co.nz>
2020-10-08 05:14:08 +1300

    Let’s try zipping, that’s a good trick

--- prep/build.sh
+++ prep/build.sh
@@ -10,6 +10,8 @@ make ${DATA_DIR}/model_data.csv
 
 mv ${DATA_DIR}/papers.csv \
    ${DATA_DIR}/papers_corpus.txt \
-   ${DATA_DIR}/fasttext.bin \
    ${DATA_DIR}/model_data.csv \
    /output
+
+zip /output/fasttext.zip \
+    ${DATA_DIR}/fasttext.bin
