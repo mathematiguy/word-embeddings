9159c35492e6b5376f387bd30399b8184e1bb616
Caleb Moses <caleb@dragonfly.co.nz>
2020-10-09 02:30:06 +1300

    run make all in prep/build.sh

--- prep/build.sh
+++ prep/build.sh
@@ -2,11 +2,12 @@
 set -ex
 
 export RUN=
-export DATA_DIR=/input/papers-past-crawler
 
 cd ..
 
-make ${DATA_DIR}/model_data.csv
+cp /input/papers-past-crawler data/papers
+
+make all
 
 # Show file sizes for output
 du -sh ${DATA_DIR}/*
@@ -17,4 +18,3 @@ mv ${DATA_DIR}/corpus.txt ${DATA_DIR}/corpus.train \
 
 zip /output/papers.zip ${DATA_DIR}/papers.csv
 zip /output/fasttext.zip ${DATA_DIR}/fasttext_cbow.bin ${DATA_DIR}/fasttext_cbow.vec
-
