6d3d965bbb9751572bf8a7a9dafb17820cfa4a64
Caleb Sawtell <caleb.sawtell@gmail.com>
2020-10-19 14:02:08 +1300

    code tidy ups

--- starmap/src/index.ts
+++ starmap/src/index.ts
@@ -1,6 +1,5 @@
 import * as THREE from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
-import * as data from "../umap.json"
 
 function docReady(fn) {
   // see if DOM is already available
@@ -12,9 +11,28 @@ function docReady(fn) {
   }
 }
 
+const loadDataAndPlaceStars = async (scene: THREE.Scene) => {
+  const {data} = await import("../umap.json");
+  data.forEach((kupu) => {
+    var geometry = new THREE.SphereGeometry(1, 6, 6);
+    var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    var sphere = new THREE.Mesh(geometry, material);
+    sphere.position.set(kupu.position[0], kupu.position[1], kupu.position[2]);
+    scene.add(sphere);
+  })
+}
 
+const initControls = (camera: THREE.Camera, domElement: HTMLCanvasElement) =>{
+  const controls = new OrbitControls(camera, domElement);
+  controls.enableZoom = false;
+  controls.enablePan = false;
+  controls.enableDamping = true;
+  controls.rotateSpeed = - 0.25;
 
-const init = () => {
+  return controls
+}
+
+const init = async () => {
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
@@ -22,48 +40,24 @@ const init = () => {
   const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
   camera.position.set(0, 0, 0.01);
 
-  const controls = new OrbitControls(camera, renderer.domElement);
-  // controls.enableZoom = false;
-  // controls.enablePan = false;
-  // controls.enableDamping = true;
-  // controls.rotateSpeed = - 0.25;
+  const controls = initControls(camera, renderer.domElement)
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(0x182332);
 
   scene.add(camera)
 
-  data.data.forEach((data)=>{
-    var geometry = new THREE.SphereGeometry(1, 6, 6);
-    var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-    var sphere = new THREE.Mesh(geometry, material);
-    sphere.position.set(data.position[0], data.position[1], data.position[2])
-    scene.add(sphere);
-  })
-
-  var geometry = new THREE.SphereGeometry(1, 6, 6);
-  var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-  var sphere = new THREE.Mesh(geometry, material);
-  sphere.position.set(0, 0, -1000);
-  scene.add(sphere);
-  console.log(scene);
-  
-
-  const animate = () => {
-
-    requestAnimationFrame(animate);
-
-    controls.update(); // required when damping is enabled
-
-    renderer.render(scene, camera);
-
-  }
-
-  animate();
-
+  return {scene, controls, renderer, camera}
 }
 
-docReady(()=>{
-  init()
+docReady(async () => {
+  const { scene, controls, renderer, camera} = await init()
+  const animate = () => {
+    requestAnimationFrame(animate);
+    controls.update(); // required when damping is enabled
+    renderer.render(scene, camera);
+  };
+  animate();
+  await loadDataAndPlaceStars(scene)
 
 });
