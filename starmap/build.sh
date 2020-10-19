9a24c746dc9e9eed6c44ec134fbe9fec416eb30c
Caleb Moses <caleb@dragonfly.co.nz>
2020-10-19 23:49:38 +1300

    Show everything under data/papers

--- starmap/build.sh
+++ starmap/build.sh
@@ -10,6 +10,8 @@ cd ..
 cp /input/papers-past-crawler/papers.json data/papers/
 cp /input/papers-past-embeddings/* data/papers/
 
+ls -la data/papers
+
 # Build the starmap front end
 make starmap
 
