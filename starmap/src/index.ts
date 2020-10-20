b6ccc7f0542369e7a06f179c1024b57b4da3fc4d
Caleb Sawtell <caleb.sawtell@gmail.com>
2020-10-20 13:27:23 +1300

    adds labels to scene

--- starmap/src/index.ts
+++ starmap/src/index.ts
@@ -19,28 +19,42 @@ function docReady(fn) {
   }
 }
 
-const buildFontMesh = (text:string, font: THREE.Font) => {
-  const matDark = new THREE.LineBasicMaterial({
-    color: WHITE,
-    side: THREE.DoubleSide
-  });
-  const fontShapes = font.generateShapes(text, 8);
-  const geometry = new THREE.ShapeBufferGeometry(fontShapes);
-  return new THREE.Mesh(geometry, matDark);
+const buildFontMesh = (text: string, font: THREE.Font, material: THREE.LineBasicMaterial): Promise<THREE.Mesh<THREE.ShapeBufferGeometry, THREE.LineBasicMaterial>> => {
+
+  return new Promise((resolve)=>{
+    const fontShapes = font.generateShapes(text, 8);
+    const geometry = new THREE.ShapeBufferGeometry(fontShapes, 1.5);
+    const mesh = new THREE.Mesh(geometry, material)
+    resolve(mesh)
+  })
 }
 
-const loadDataAndPlaceStars = async (scene: THREE.Scene) => {
-  const { data } = await import("../../data/papers/umap.json");
-  const font = await initFont();
+type FontMesh = THREE.Mesh<THREE.ShapeBufferGeometry, THREE.LineBasicMaterial>
 
+const kupuLabelMap: Record<string, FontMesh> = {}
 
-  data.forEach((kupu: Kupu) => {
+const buildKupuLabels = async (scene: THREE.Scene, kupuData: Kupu[], font: THREE.Font) => {
+  const matDark = new THREE.LineBasicMaterial({
+    color: WHITE,
+    side: THREE.FrontSide
+  });
+  kupuData.forEach( async (kupu: Kupu) => {
+    if (kupuLabelMap[kupu.word]){
+      return
+    }
+    const text = await buildFontMesh(kupu.word.replace(/_/g, " "), font, matDark);
+    text.position.set(kupu.position[0] + 10, kupu.position[1], kupu.position[2])
+    scene.add(text);
+    kupuLabelMap[kupu.word] = text
+  })
+}
+
+const loadDataAndPlaceStars = async (scene: THREE.Scene, kupuData: Kupu[]) => {
+  kupuData.forEach((kupu: Kupu) => {
     var geometry = new THREE.SphereGeometry(1, 6, 6);
     var material = new THREE.MeshBasicMaterial({ color: WHITE });
     var sphere = new THREE.Mesh(geometry, material);
-    // const text = buildFontMesh(kupu.word.replace(/_/g, " "), font);
-    // text.position.set(kupu.position[0] + 10, kupu.position[1], kupu.position[2])
-    // scene.add(text);
+
     sphere.position.set(kupu.position[0], kupu.position[1], kupu.position[2]);
     scene.add(sphere);
   })
@@ -85,18 +99,36 @@ const init = async () => {
   scene.background = new THREE.Color(NAVY);
   scene.add(camera)
 
-
   return {scene, controls, renderer, camera}
+}
+
+const kupuInView = (camera: THREE.Camera, kupuData: Kupu[], controls: OrbitControls) => {
+  const frustum = new THREE.Frustum;
+  const baseMatrix = camera.projectionMatrix.clone()
+  frustum.setFromProjectionMatrix(baseMatrix.multiply(camera.matrixWorldInverse.clone()));
+  return kupuData.filter( 
+    (kupu) => (
+      frustum.containsPoint( new THREE.Vector3(kupu.position[0], kupu.position[1], kupu.position[2]) )
+    )
+  )
 }
 
 docReady(async () => {
   const { scene, controls, renderer, camera} = await init()
+  const umap = await import("../../data/papers/umap.json");
+  const font = await initFont();
+  const kupuData: Kupu[] = umap.data as Kupu[]
+
+  await loadDataAndPlaceStars(scene, kupuData)
   const animate = () => {
     requestAnimationFrame(animate);
     controls.update(); // required when damping is enabled
     renderer.render(scene, camera);
+    if (camera.fov < 7) {
+      const visibleKupu = kupuInView(camera, kupuData, controls)
+      buildKupuLabels(scene, visibleKupu, font)
+    }
   };
   animate();
-  await loadDataAndPlaceStars(scene)
 
 });
