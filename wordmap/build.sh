2f7dd75dfcd82549c7b02ca26c4da0187b24b10d
Caleb Moses <calebjdmoses@gmail.com>
2020-11-01 12:21:52 +1300

    Prepare wordmap

--- wordmap/build.sh
+++ wordmap/build.sh
@@ -11,7 +11,7 @@ unzip /input/papers-past-embeddings/te_ara.zip -d .
 
 tree data
 
-make wordmap -tB
-make wordmap
+make wordmap/papers/umap.csv -tB
+make wordmap/te_ara/umap.csv -tB
 
 cp -r papers te_ara /publish
