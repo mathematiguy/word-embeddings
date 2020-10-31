745621c09d814f885e42bd76d95d6a5af0ffa758
Caleb Moses <calebjdmoses@gmail.com>
2020-11-01 12:31:45 +1300

    Fix make wordmap in build.sh

--- wordmap/build.sh
+++ wordmap/build.sh
@@ -11,7 +11,10 @@ unzip /input/papers-past-embeddings/te_ara.zip -d .
 
 tree data
 
-make wordmap/papers/umap.csv -tB
-make wordmap/te_ara/umap.csv -tB
+make data/papers/umap.csv -tB
+make data/te_ara/umap.csv -tB
+
+make wordmap/papers/umap.csv
+make wordmap/te_ara/umap.csv
 
 cp -r wordmap/papers wordmap/te_ara /publish
